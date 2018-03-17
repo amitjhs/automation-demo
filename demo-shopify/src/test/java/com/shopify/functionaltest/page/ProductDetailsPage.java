17ad762b2b9eb513162b75b26cf24fb42dde8b32
Alok Arora <alok.arora@decurtis.com>
2018-03-17 18:39:31 +0530

    Initial Commit

---
+++ demo-shopify/src/test/java/com/shopify/functionaltest/page/ProductDetailsPage.java
@@ -0,0 +1,30 @@
+package com.shopify.functionaltest.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductDetailsPage {
+
+	private WebDriver driver;
+	
+	@FindBy(xpath = "")
+	private WebElement _local;
+
+	// Default Constructor
+	public ProductDetailsPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public boolean isLoaded() {
+		{
+			if (driver.getTitle() == "Product Details")
+				return true;
+			else
+				return false;
+		}
+	}
+
+}
